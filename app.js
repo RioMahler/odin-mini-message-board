b973ab49c0b0d8dfce8ef65e18b970099235b74d
RioMahler <229354130+RioMahler@users.noreply.github.com>
2026-03-06 12:15:49 +0100

    Add basic routing

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
+
+app.use("/", indexRouter);
+app.use("/new", newRouter);
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on port ${PORT}!`);
+});
