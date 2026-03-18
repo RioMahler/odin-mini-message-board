bb759d68084f2634496f98f353f8400ae206c4e1
RioMahler <229354130+RioMahler@users.noreply.github.com>
2026-03-18 17:34:17 +0100

    Change back to connection string for connection

--- db/createTable.js
+++ db/createTable.js
@@ -5,19 +5,14 @@ CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   username VARCHAR ( 255 ),
   message VARCHAR ( 255 ),
-  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 `;
-console.log("DB_HOST:", process.env.DB_HOST);
+
 async function populateDB() {
   console.log("seeding...");
   const client = new Client({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
+    connectionString: process.env.DATABASE_URL,
   });
   await client.connect();
   await client.query(SQL);
