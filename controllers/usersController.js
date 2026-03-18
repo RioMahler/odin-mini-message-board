ed055e2526f8f29e32ca6486848abf58ce84017b
RioMahler <229354130+RioMahler@users.noreply.github.com>
2026-03-18 16:23:37 +0100

    Add simple form validation

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,21 @@
 const populateDB = require("../db/createTable");
 const db = require("../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 255 characters.";
+const validateUser = [
+  body("username")
+    .trim()
+    .isAlpha()
+    .withMessage(`username ${alphaErr}`)
+    .isLength({ min: 1, max: 255 })
+    .withMessage(`username ${lengthErr}`),
+  body("message")
+    .trim()
+    .isLength({ max: 200 })
+    .withMessage(`Message ${lengthErr}`),
+];
 
 async function getMessages(req, res) {
   populateDB();
@@ -8,11 +24,21 @@ async function getMessages(req, res) {
   console.log(messages);
 }
 
-async function createMessagesPost(req, res) {
-  const { username, message } = req.body;
-  await db.insertMessages(username, message);
-  res.redirect("/");
-}
+const createMessagesPost = [
+  validateUser,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("new", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
+    const { username, message } = matchedData(req);
+    await db.insertMessages(username, message);
+    res.redirect("/");
+  },
+];
 
 async function getIndividualMessage(req, res) {
   const { postId } = req.params;
